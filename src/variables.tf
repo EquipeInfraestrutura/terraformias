611197044842de1401d175e4c5607b84ba42272b
EquipeInfraestrutura <equipe.infraestrutura.ias.org.br>
2023-01-12 15:41:12 -0300

    nova remoção de variáveis

--- src/variables.tf
+++ src/variables.tf
@@ -1,16 +1,3 @@
-#variable "aws_access_key" {
-#  type = string
-#}
-
-#variable "aws_secret_key" {
-#  type = string
-#}
-
 variable "access_ip" {
   type = string
-}
-
-variable "aws_region" {
-  type    = string
-  default = "us-east-2"
 }
