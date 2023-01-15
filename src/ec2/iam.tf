c229e4b4ca89601b09b5bdba38f3a808b951d32e
EquipeInfraestrutura <equipe.infraestrutura.ias.org.br>
2023-01-14 21:53:44 -0300

    path

--- src/ec2/iam.tf
+++ src/ec2/iam.tf
@@ -10,6 +10,7 @@ resource "aws_iam_role_policy" "ec2_policy" {
 resource "aws_iam_role" "labdados_ec2_role" {
   name = "labdados_ec2_role"
   assume_role_policy = "${file("ec2-assume-policy.json")}"
+  path = "/"
 }
 
 resource "aws_iam_instance_profile" "ec2_profile" {
