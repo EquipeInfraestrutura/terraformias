07d3dffc127ae952a58bf67be95844b97b23bae3
EquipeInfraestrutura <equipe.infraestrutura.ias.org.br>
2023-01-16 10:42:55 -0300

    troca de access key e secret key

--- src/ec2/main.tf
+++ src/ec2/main.tf
@@ -14,57 +14,55 @@ resource "aws_key_pair" "key_pair" {
   }
 }
 
+#resource "aws_iam_role" "ec2_labados_role" {
+  #name = "ec2_labados_role"
+  #path = "/"
 
-resource "aws_iam_role" "ec2_labados_role" {
-  name = "ec2_labados_role"
-  path = "/"
+  #assume_role_policy = jsonencode({
+    #Version = "2012-10-17"
+    #Statement = [
+      #{
+        #Action = "sts:AssumeRole"
+        #Effect = "Allow"
+        #Sid    = ""
+        #Principal = {
+          #Service = "ec2.amazonaws.com"
+        #}
+      #},
+    #]
+  #})
+#}
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
-
-resource "aws_iam_role_policy" "ec2_labdados_policy" {
-  name = "ec2_labdados_policy"
-  role = aws_iam_role.ec2_labados_role.id
+#resource "aws_iam_role_policy" "ec2_labdados_policy" {
+  #name = "ec2_labdados_policy"
+  #role = aws_iam_role.ec2_labados_role.id
 
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ec2:Describe*",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
-}
+ # policy = jsonencode({
+  #  Version = "2012-10-17"
+   # Statement = [
+    #  {
+     #   Action = [
+      #    "ec2:Describe*",
+       # ]
+        #Effect   = "Allow"
+        #Resource = "*"
+      #},
+    #]
+  #})
+#}
 
-resource "aws_iam_instance_profile" "ec2_labdados_profile" {
-  name = "ec2_labdados_profile"
-  role = aws_iam_role.ec2_labados_role.name
-}
+#resource "aws_iam_instance_profile" "ec2_labdados_profile" {
+  #name = "ec2_labdados_profile"
+  #role = aws_iam_role.ec2_labados_role.name
+#}
 
 
 # Create a EC2 Instance (Windows Server 2022)
 resource "aws_instance" "windows" {
   instance_type          = "m5n.xlarge"
   ami                    = data.aws_ami.windows.id
-  iam_instance_profile   = aws_iam_instance_profile.ec2_labdados_profile.name
   key_name               = aws_key_pair.key_pair.id
   vpc_security_group_ids = [var.public_sg]
   subnet_id              = var.public_subnet
