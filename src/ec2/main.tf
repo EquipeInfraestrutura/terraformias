f8707f43e70eba51b2249ccf82137cb4b922ed04
EquipeInfraestrutura <equipe.infraestrutura.ias.org.br>
2023-01-16 15:11:33 -0300

    Ajustes 16-01 15:11

--- src/ec2/main.tf
+++ src/ec2/main.tf
@@ -1,69 +1,8 @@
-# Get Availability Zones
-resource "tls_private_key" "key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-# Generate a Private Key and encode it as PEM.
-resource "aws_key_pair" "key_pair" {
-  key_name   = "Key_BI"
-  public_key = tls_private_key.key.public_key_openssh
-
-  provisioner "local-exec" {
-    command = "echo '${tls_private_key.key.private_key_pem}' > ./key.pem"
-  }
-}
-
-#resource "aws_iam_role" "ec2_labados_role" {
-  #name = "ec2_labados_role"
-  #path = "/"
-
-  #assume_role_policy = jsonencode({
-    #Version = "2012-10-17"
-    #Statement = [
-      #{
-        #Action = "sts:AssumeRole"
-        #Effect = "Allow"
-        #Sid    = ""
-        #Principal = {
-          #Service = "ec2.amazonaws.com"
-        #}
-      #},
-    #]
-  #})
-#}
-
-#resource "aws_iam_role_policy" "ec2_labdados_policy" {
-  #name = "ec2_labdados_policy"
-  #role = aws_iam_role.ec2_labados_role.id
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
- # policy = jsonencode({
-  #  Version = "2012-10-17"
-   # Statement = [
-    #  {
-     #   Action = [
-      #    "ec2:Describe*",
-       # ]
-        #Effect   = "Allow"
-        #Resource = "*"
-      #},
-    #]
-  #})
-#}
-
-#resource "aws_iam_instance_profile" "ec2_labdados_profile" {
-  #name = "ec2_labdados_profile"
-  #role = aws_iam_role.ec2_labados_role.name
-#}
-
-
 # Create a EC2 Instance (Windows Server 2022)
 resource "aws_instance" "windows" {
   instance_type          = "m5n.xlarge"
   ami                    = data.aws_ami.windows.id
-  key_name               = aws_key_pair.key_pair.id
+  key_name               = "Key_Labdados"
   vpc_security_group_ids = [var.public_sg]
   subnet_id              = var.public_subnet
 
