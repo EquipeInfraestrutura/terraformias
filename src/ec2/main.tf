02eba588beb489dbdc89f2ab134e0e5f7e0639f7
EquipeInfraestrutura <equipe.infraestrutura.ias.org.br>
2023-01-16 09:09:18 -0300

    ajuste labdados

--- src/ec2/main.tf
+++ src/ec2/main.tf
@@ -63,7 +63,7 @@ resource "aws_iam_instance_profile" "ec2_labdados_profile" {
 resource "aws_instance" "windows" {
   instance_type          = "m5n.xlarge"
   ami                    = data.aws_ami.windows.id
-  iam_instance_profile   = "${aws_iam_instance_profile.ec2_profile.name}"
+  iam_instance_profile   = aws_iam_instance_profile.ec2_labdados.name
   key_name               = aws_key_pair.key_pair.id
   vpc_security_group_ids = [var.public_sg]
   subnet_id              = var.public_subnet
